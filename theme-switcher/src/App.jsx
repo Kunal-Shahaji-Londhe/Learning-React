bbd91451623bf926238e4199421180cc9059fb52
Kunal Londhe <kunal.shahaji.londhe@gmail.com>
2024-01-11 19:27:06 +0530

    added theme-switcher context project

---
+++ theme-switcher/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Themeprovider } from './contexts/theme'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
+
+function App() {
+
+  const [themeMode, setThemeMode] = useState("light")
+  
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  //actual change in theme
+
+  useEffect( () => {
+    document.querySelector('html').classList.remove("light","dark")
+  
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode]) 
+
+  return (
+    
+      <Themeprovider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+                  <div className="w-full">
+                      <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                          <ThemeButton/>
+                      </div>
+
+                      <div className="w-full max-w-sm mx-auto">
+                        <Card/>
+                      </div>
+                  </div>
+        </div>
+      </Themeprovider>
+
+  )
+}
+
+export default App
